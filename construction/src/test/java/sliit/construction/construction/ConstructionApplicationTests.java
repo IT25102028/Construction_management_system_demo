094e7ec12edf7f85c686e22e69b201be9b392ef9
IT25102037 <IT25102037@my.sliit.lk>
2026-09-10 23:20:42 +0530

    Add construction management system project

---
+++ construction/src/test/java/sliit/construction/construction/ConstructionApplicationTests.java
@@ -0,0 +1,13 @@
+package sliit.construction.construction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
